b72d3f74ab6c14beb6cfbdc68aa0fc816b3d5eb0
aman13122001 <amankumar13122001@gmail.com>
2025-11-03 18:50:39 +0530

    Add loop,Function,String

---
+++ JavaScript/Function/script.js
@@ -0,0 +1,137 @@
+// 1.What is a function?
+// Ans:- Block of code,and reuse the code.
+
+// function greet(){
+
+//     console.log("hello World");
+
+// }
+
+// function call
+// greet();
+// greet();
+// greet();
+// greet();
+// greet();
+
+// pass Parameter to a function
+// function greet(value){
+
+//     console.log(value);
+
+// }
+
+// greet('hey');
+// greet('hello');
+// greet('halua');
+// greet('ghoda');
+// greet('nemaste');
+
+// function add(a,b,c,d){
+
+//     console.log(a+b+c+d);
+
+// }
+// -: pass Argument to a function:-
+// add(1,20,3,5);
+
+// function add(a,b,c,d){
+
+//     console.log(a,b,c,d);
+
+// }
+
+// add(1,2,3);
+
+// Type of function:-
+// There are six type of function
+// 1.Function Statement
+
+// function abd(params) {
+//     //function Statement
+// }
+
+// 2. function expression
+// var abcd = function () {
+//     // fun expression
+// }
+
+// 3. anonymous function
+
+// function () {
+//     //anonymous function
+// }
+
+// 4. fat arrow function:-
+
+// ()=>{
+
+// }
+
+// 5.fat arrow with one parameter function :-
+
+// let abcd = a =>{
+
+// }
+
+// abcd(12);
+
+// 6.fat arrow with implicit return
+
+// var abcd = ()=>'Aman';
+// var ans = abcd();
+// console.log(ans);
+
+// Return the function:-
+
+// function abcd(){
+//     return 12;
+// }
+
+// let ans = abcd();
+// console.log(ans);
+
+// rest parameter:-
+
+// function abcd(a, b, c, ...rest) {
+//   console.log(a, b, c, rest);
+// }
+
+// abcd(1, 2, 3, 4, 5, 6, 7, 8);
+
+
+
+// hoisting:- js mein variables and functions unko js declaration waala part top pe move kr deta hai aur isey hu kahte hai hositing.
+
+// variable Hositing:-
+
+// console.log(a);
+// var a=12;
+
+
+// function Hositing:-
+
+// abcd();
+
+// function abcd(){
+//     console.log("Hello World");
+    
+// }
+
+
+
+// iife:- immediatety invoked function expression.
+
+// Ex:-1
+
+// (function abcd(){
+//     console.log("iife");
+
+// })();
+
+
+// Ex:-2
+
+
+
+
---
+++ JavaScript/StringAndLoop/script.js
@@ -0,0 +1,133 @@
+// -:String:-
+
+// 1.slice
+
+// let str="Apple";
+// let ans =str.slice(0,4)
+// console.log(ans);
+
+// ` `:- backticks, template , template String
+
+// let str= `het  my age is ${12+2}`;
+// console.log(str);
+
+
+// 2.Split:-
+
+// let str = "Hello ji kaise ho"
+// let ans = str.split('ai')
+// console.log(ans);
+
+
+// 3.replace:-
+
+// let str = "Hello ji kaise ho"
+// let ans1= str.replace('i','y')
+// console.log(ans1);
+// let ans2 = str.replaceAll('i','y')
+// console.log(ans2);
+
+// 4.includes
+
+// let str= "Hello, World!";
+// let ans = str.includes("W");
+// console.log(ans);
+
+
+// Condition:-
+
+// 1.Ternart Operater
+
+// let x=2
+// let result = x>5? "Big":"Small";
+// console.log(result);
+
+// 2.Switch
+
+// switch (3) {
+//     case 1:
+//         console.log('One');
+//         break;
+//     case 2:
+//         console.log('Two');
+//         break;
+//     case 3:
+//         console.log('There');
+//         break;
+//     case 4:
+//         console.log('Four');
+//         break;
+
+//     default:
+//         console.log('Default');
+        
+// }
+
+
+// -:Loop:-
+
+ 
+// 1. For loop:-
+
+// Ex:-
+
+// for(let i=0;i<5;i++){
+//     console.log(i);
+    
+// }
+
+
+// Dynamica loop
+
+// let arr = [1,2,3,4,5,6,7,8,'A'];
+
+// for(let i=0;i<arr.length;i++){
+//       console.log(arr[i]);
+      
+// }
+
+
+// 2.while
+
+// let i=1;
+// while(i<10){
+//     console.log(i);
+//     i++;
+    
+// }
+
+
+// 3.do while:-
+
+// let i=12;
+
+// do{
+//     console.log("Hello");
+//     i++;
+    
+// }while(i>20)
+
+
+//1.break:-
+
+// for(let i=0;i<15;i++){
+//     console.log(i);
+//     if(i===10) break;
+    
+// }
+
+//2.Continue  
+
+// for(let i=0;i<15;i++){
+//     if(i===10) continue;
+//     console.log(i);
+    
+    
+// }
+
+
+
+
+
+
+
