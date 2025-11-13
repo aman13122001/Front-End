e6a0be2fa77f414c8a3fe2a0e13c0c1df6f039eb
aman13122001 <amankumar13122001@gmail.com>
2025-11-13 07:07:11 +0530

    Add Introduction of Arrays And Object

---
+++ JavaScript/ArrayAndObject/Arrays/script.js
@@ -0,0 +1,51 @@
+// Arrays 
+
+// var arr =[12,13,14];
+
+// What is Array?
+// Array is the Data Structure Which can store multiple  values all at one place.
+
+// How to use Array?
+
+// How to create array 
+
+// method-1 :- by using Squre brackets[].
+
+// let fruits =['Apple','banana','manago'];
+
+
+// method-2 :- using new Array().
+
+// let numbers = new Array(10,20,30,40);
+
+// let arr2 = new Array(100);
+
+
+// let arr = new Array(30);
+
+// for (let i =0; i<30;i++){
+//     arr[i]=i;
+// }
+
+
+
+// How to access members of Array?
+// by using indexing.
+
+
+// let fruits =['Apple','banana','manago'];
+
+// console.log(fruits[0]);
+// console.log(fruits[2]);
+
+
+
+
+// Note:- In JS Array is not homogeneous, you can store multiple type of values in an Array. 
+// var arr = ['a',12,function () {},{},[],NaN,undefined]
+
+
+
+
+
+
---
+++ JavaScript/ArrayAndObject/Objects/script.js
@@ -0,0 +1,52 @@
+// What is object?
+// Ans:-An object in JavaScript is a collection of data (properties) and functions (methods) stored together in one structure.
+// An object stores data in key–value pairs.
+// Why Object
+// When to make an object
+// how to create an Object
+// how to access an object
+
+// Object Syntex
+// var obj = {
+//     property:value
+// };
+
+// Ex:-
+
+// var obj = {
+//     name:"Aman",
+//     age: 23,
+//     email: "amankumar1312@gmail.com"
+    
+// };
+
+
+// When to make an object-> when you want to store the  info of one entity.
+
+// how to create an object
+
+// method-1 :- by using curly brackets {}.
+
+// var obj = {};
+
+// method-2 :- using new Object().
+
+// var obj2 = new Object();
+
+// How to access an object.
+
+
+var obj = {
+    name:"Aman",
+    age: 23,
+    email: "amankumar1312@gmail.com"
+    
+};
+
+console.log(obj.email);
+console.log(obj.age);
+
+
+
+ 
+
