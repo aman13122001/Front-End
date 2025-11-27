c139bc3a7ad9e323f1e78d32091a05a64996be57
aman13122001 <amankumar13122001@gmail.com>
2025-11-28 02:06:23 +0530

    add Array and Object lerning

--- JavaScript/ArrayAndObject/Arrays/script.js
+++ JavaScript/ArrayAndObject/Arrays/script.js
@@ -45,7 +45,293 @@
 // var arr = ['a',12,function () {},{},[],NaN,undefined]
 
 
+// let arr = [10,20,30,40];
 
+// console.log(arr); //  [10, 20, 30, 40]
+
+// console.log(arr[0]); //10
+// console.log(arr[1]); //20
+// console.log(arr[2]); //30
+// console.log(arr[3]); //40
+
+
+// let arr = new Array()
+
+// arr[0]=10;
+// arr[2]=20;
+// arr[3]=30;
+// arr[4]=40;
+
+// console.log(arr);//(5) [10, empty, 20, 30, 40]
+
+
+// Array Methods
+
+// let arr= [1,2,3,4,5];
+// 1.puch Method
+// arr.push(10);
+// console.log(arr);
+
+// 2.pop Method
+// arr.pop();
+// console.log(arr); //[1, 2, 3, 4]
+
+// 3. shift() Method
+// arr.shift();
+// console.log(arr); //[2, 3, 4, 5]
+
+// 4. unshift() Method
+// arr.unshift(0);
+// console.log(arr); //[0, 1, 2, 3, 4, 5]
+
+// 5.splice() method:
+// arr.splice(1,2);
+// console.log(arr); //[1, 4, 5] Remove 2 items starting at index 1
+                 
+// 6.reverse():- Reverse order
+// arr.reverse();
+// console.log(arr); //[5, 4, 3, 2, 1]
+
+// 7. map() Method:- Creates a new array by applying a function to each element.
+
+// let nums = [1,2,3];
+// let doubled = nums.map(n=> n*2);
+
+// console.log(doubled); //2, 4, 6]
+
+// 8.reduce() Method:
+
+// let nums=[1,2,3];
+// let sum = nums.reduce((acc,n)=>acc+n , 0);
+// console.log(sum); //6
+
+// 9. find(): Returns the first value that matches a condition.
+
+// let arr = [10,20,30];
+
+// let result = arr.find(x=> x>15)
+// console.log(result); //20
+
+// 10.findIndex():Returns the index of the first match.
+
+// let arr =[5,12,8];
+// let index = arr.findIndex(x=> x>10);
+// console.log(index); //1
+
+
+// 11.includes(): Checks if a value exists.
+
+// let arr = [1, 2, 3];
+// let bool= arr.includes(2);
+// console.log(bool); //true
+
+
+// 12. sort(): Sortd the array.
+
+// let arr =[3,1,2];
+// let sortArr= arr.sort();
+// console.log(sortArr); //[1, 2, 3]
+
+// 13. concat():Joins two arrays.
+
+// let a = [1, 2];
+// let b = [3, 4];
+// let c = a.concat(b);
+// console.log(c); // [1, 2, 3, 4]
+
+// 14. slice():Returns a part of the array (does NOT change original).
+
+// let arr = [1, 2, 3, 4];
+// let sliceArr = arr.slice(1, 3); 
+// console.log(sliceArr); // [2, 3]
+
+// 15. splice(): Adds/removes items (changes original).
+
+// let arr = [1, 2, 3, 4];
+// let newArr= arr.splice(1, 2);
+// console.log(soliceArr);
+// removes 2 items starting at index 1 → [2, 3]
+// new arr = [1, 4]
+
+
+// forEach():
+
+// let arr= [1,2,3,4];
+
+// arr.forEach(function(val){
+//     console.log(val*2);
+    
+// })
+
+// map():
+
+// let arr=[1,2,3,4]
+// let newarr = arr.map(function(val){
+//     return val; 
+// });
+// console.log(newarr); //[1, 2, 3, 4]
+
+
+// filter():
+
+
+// let arr =[1,2,3,4,5,6,7,8];
+
+// let ans = arr.filter(function(val){
+//     if(val>4) {
+//         return true
+//     }
+// })
+// console.log(ans); //[5, 6, 7, 8]
+
+// reduce():
+
+// let arr = [1,2,3,4,5,6];
+
+// let ans=arr.reduce(function(acc,val){
+//     return acc+val;
+// },0);
+// console.log(ans); //21
+
+
+// Some():-
+
+
+// let arr = [10,28,67,89,50,60];
+
+// let any = arr.some(function(val){
+//     return val>85;
+// });
+// console.log(any); //true
+
+// every():
+
+// let arr = [40,55,60,67,80];
+
+// let Every = arr.every(function(val){
+//     return val>39;
+// })
+
+// console.log(Every); //true
+
+
+// Destructring:
+
+// let arr = [1,2,3,4,5,6];
+// let [a,b, ,c]=arr;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Sprad Operator:
+
+// let arr =[1,2,3,4]
+// let arr2= [...arr]
+// arr2.pop()
+// console.log(arr); // [1, 2, 3, 4]
+// console.log(arr2); // [1, 2, 3]
+
+// Add mango at the end and pineapple at beginning of this Array
+
+// let fruits = ["Apple","BAnana"];
+
+// fruits.push("Manago");
+// fruits.unshift("Pineapple");
+
+// console.log(fruits); // ['Pineapple', 'Apple', 'BAnana', 'Manago']
+
+// Replace "Banana" with "kiwi " in the array above.
+// let fruits = ["Apple","BAnana"];
+// fruits.pop();
+// fruits.push("kiwi");
+// console.log(fruits); //['Apple', 'kiwi']
+
+
+// Insert "Red" and "Blue" at index 1 and 2 in this array
+
+// let colors = ["green","yellow"];
+// colors.splice(1,0,"Red", "Blue");
+// console.log(colors); //['green', 'Red', 'Blue', 'yellow']
+
+// Extract only the middle 3 elements from this array.
+// let items =[1,2,3,4,5,6];
+
+// let newarr=items.slice(2,4)
+// console.log(newarr); //[3, 4]
+
+// Sort this array alphaabetically and then reverse it:
+
+// let names = ["zara","Arjun","Mira","Bhavya"];
+// names.sort().reverse();
+// console.log(names); //['zara', 'Mira', 'Bhavya', 'Arjun']
+
+
+// Use .map() to square each number:
+
+// let nums = [1,2,3,4,5];
+
+// let square = nums.map(function(val){
+//     return val*val;
+// });
+// console.log(square); //[1, 4, 9, 16, 25]
+
+
+// Use .filter() to keep numbers greater then 10.
+
+// let arr = [2,3,5,13,45,67,10];
+
+// let newarr= arr.filter(function(val){
+//      return val>=10;
+// });
+// console.log(newarr); //[13, 45, 67, 10]
+
+// Use .reduce() to find th sum of this arrays:
+
+// let arr = [10,20,30];
+
+// let Sum= arr.reduce(function(acc,val){
+//     return acc+val;
+// },0);
+// console.log(Sum);  //60
+
+// Use .find() to get the first number less then 10.
+
+// let arr = [2,3,5,13,45,67,10];
+
+// let num= arr.find(val=>{
+//   return val<10;
+// })
+
+// console.log(num); //2
+
+
+// Use .some() to check if any student has scored below 35:
+
+// let arr = [45,60,28,90];
+
+// let ans= arr.some(function(val){
+//     return val<35;
+// })
+
+// console.log(ans); //true
+
+
+// Use .every to check if all number are even:
+
+// let num = [2,4,6,8,10];
+
+// let ans = num.every(function(val){
+//     return val%2===0
+// })
+// console.log(ans); //true
+
+
+// Merge two Arrays using spread operator:
+
+// let a= [1,2];
+// let b=[3,4];
+// let c= [...a,...b]
+// console.log(c); //[1, 2, 3, 4]
 
 
 
--- JavaScript/ArrayAndObject/Objects/script.js
+++ JavaScript/ArrayAndObject/Objects/script.js
@@ -36,17 +36,347 @@
 // How to access an object.
 
 
-var obj = {
-    name:"Aman",
-    age: 23,
-    email: "amankumar1312@gmail.com"
+// var obj = {
+//     name:"Aman",
+//     age: 23,
+//     email: "amankumar1312@gmail.com"
     
-};
+// };
 
-console.log(obj.email);
-console.log(obj.age);
+// console.log(obj.email);
+// console.log(obj.age);
+
+
+// Create a object
+// let student ={
+//     name:"Aman",
+//     age :21,
+//     isEnrolled:true
+// }
+// how to access the object items/Values
+// Dot vs Bracket notation
+// console.log(student.name); //Aman
+// console.log(student['age']); //21
+// console.log(student.isEnrolled); //true
+
+// Nesting and Deep Access:
+
+// Nesting
+// let user ={
+//     name:"Aman",
+//     address:{
+//         city:"Noida",
+//         pin:208010,
+//         location:{
+//             lag:"c220",
+//             leg:"c216"
+
+//         },
+//     },
+// };
+
+// // Deep Access
+// console.log(user.address.location.leg); //c216
+ 
+
+// Object Destructuring:
+// let user ={
+//     name:"Aman",
+//     address:{
+//         city:"Noida",
+//         pin:208010,
+//         location:{
+//             lag:"c220",
+//             leg:"c216"
+
+//         },
+//     },
+// };
+// let {lag,leg}=user.address.location;
+
+// console.log(lag); //c220
+// console.log(leg); //c216
+
+// loops throygh object:
+
+// for-in loop:-
+
+// let student ={
+//     name:"Aman",
+//     age :21,
+//     email: "aman1312@hmail.com"
+// }
+
+// for(let key in student){
+//     console.log(key);   
+    
+// }
+// output :name
+//         age
+//         email
+
+// for(let key in student){
+//     console.log(key,student[key]);
+    
+// }
+// output :
+// name Aman
+// age 21
+// email aman1312@hmail.com
+
+
+// Object.key , Object.values(), Object.entries()
+
+
+//    
+
+// console.log(Object.keys(student)); //['name', 'age', 'email']
+// console.log(Object.values(student)); //['Aman', 21, 'aman1312@hmail.com']
+// console.log(Object.entries(student)); //[Array(2), Array(2), Array(2)]
+
+//Copying Objects
+
+//1. Copying Objects by using Spread operater.
+
+// let student ={
+//     name:"Aman",
+//     age :21,
+//     email: "aman1312@hmail.com"
+// }
+
+
+// let student2 = {...student};
+// console.log(student); //both ouput are same
+// console.log(student2);
+
+// 2.Copying Objects by using Object.assign
+
+// let student ={
+//     name:"Aman",
+//     age :21,
+//     email: "aman1312@hmail.com"
+// }
+
+// let student2 = Object.assign({},student);
+// console.log(student2); //{name: 'Aman', age: 21, email: 'aman1312@hmail.com'}
+
+
+// you can also add the keys value pair.
+
+// let student ={
+//     name:"Aman",
+//     age :21,
+//     email: "aman1312@hmail.com"
+// }
+
+// let student2 = Object.assign({city:"Noida"},student);
+// console.log(student2); //{city: 'Noida', name: 'Aman', age: 21, email: 'aman1312@hmail.com'}
+
+
+// Deep Clone:- You create a new copy of an object or array including all nested objects or arrays, so the new copy is totally independent from the original.
+
+
+//1.Using JSON method (simple deep clone)
+
+
+// const original = {
+//     name: "Aman",
+//     address:{
+//         city:"delhi",
+//         pin:201902
+//     }
+// };
+
+// const deepClone = JSON.parse(JSON.stringify(original));
+
+// deepClone.address.city="mumbai";
+
+// console.log(original.address.city); // delhi
+// console.log(deepClone.address.city); // mumbai
+
+// 2. Better Deep Clone (using structuredClone)
+
+// const original = {
+//     name: "Aman",
+//     address:{
+//         city:"delhi",
+//         pin:201902
+//     }
+// };
+
+// const deepClone = structuredClone(original)
+
+// deepClone.address.city="mumbai";
+
+// console.log(original.address.city); // delhi
+// console.log(deepClone.address.city); // mumbai
 
 
 
+// Optional Chaining: It help to Avoids errors ,if a nested property is undefined. 
+
+// const person = {
+//     name: "Aman",
+//     address:{
+//         city:"delhi",
+//         pin:201902
+//     }
+// };
+
+// console.log(person?.address?.city); //delhi
+// console.log(person?.profile?.city); //undefind
+
+
+// 1. Create an Object for a student with nume, age and isEnrolled.
+
+// let obj ={
+//     name:"Aman",
+//     age:"24",
+//     isEnrolled : true
+// };
+
+// Use Object.entries() to print all key-value pairs as:
+// title:JavaScript
+// Duration :4 weeks
+
+// const course ={
+//     title: "JavaScript",
+//     duration:"4 Weeks",
+// };
+
+// Object.entries(course).forEach(function(val){
+//     console.log(val[0] +": "+val[1]);
+    
+// })
+
+
+// Output:
+
+// title: JavaScript
+// duration: 4 Weeks
+
+// Timing Events 
+
+// setTimeout(): Runs a function once, after a given time.
+
+// Syntax:-
+// setTimeou(functiontimeInMilliseconds);
+
+// setTimeout(()=>{
+//     console.log("Hello Shivansh!");
+    
+// },2000)
+
+
+// 2. setInterval(): Runs a function again and again after a specific time gap.
+
+// Syntax
+// setInterval(function,timeInMilliseconds);
+
+
+// setInterval(()=>{
+//     console.log("Repeating every 1 second");
+    
+// },1000);
+
+// 3. clearTimeout():Stops a setTimeout() before it runs.
+
+// Ex:
+
+// let timer = setTimeout(()=>{
+//     console.log("This will not run");
+    
+// },1000);
+// clearTimeout(timer); //cancel timer
+
+
+// 4.clearInterval():Stops a running setInterval() loop.
+
+// let count =0;
+// let interval = setInterval(()=>{
+//     count++;
+//     console.log(count);
+
+//     if(count===5){
+//         clearInterval(interval); //stop after 5 times
+//     }
+    
+// },1000);
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// (Then stops.)
+
+// Operation in Objects:-
+
+// 1. Object.freeze():Freezes an object. You cannot add, delete, or change any property.
+
+// Ex:-
+
+// let user ={
+//     name:"Shivansh",
+//     age:21
+// };
+// Object.freeze(user);
+// user.age = 30;    // ❌ no change
+// user.city = "Patna";// ❌ cannot add
+// delete user.name; // ❌ cannot delete
+
+// console.log(user); 
+
+// Output: { name: "Shivansh", age: 21 }
+
+// 2. Object.seal():-
+// You cannot add or delete properties,
+// BUT you can modify existing properties.
+
+// let user ={
+//     name:"Shivansh",
+//     Age:24
+// };
+
+// Object.seal(user);
+
+// user.age = 22;      // ✔ allowed
+// user.city = "Patna"; // ❌ cannot add
+// delete user.name;    // ❌ cannot delete
+
+// console.log(user);
+
+// Output:
+// {name: 'Shivansh', Age: 24}
+
+
+// Object Destructuring: Destructuring means extracting values from an object into variables.
+
+// let user = {
+//     name: "Aman",
+//     age: 21,
+//     city: "Patna"
+//   };
+  
+//   let { name, age } = user;
+  
+//   console.log(name); // Aman
+//   console.log(age);  // 21
+
+// this Keyword in JavaScript: Inside an object, this refers to that object.
+
+// let user ={
+//     name:"Aman",
+//     age:21,
+//     greet(){
+//         console.log("Hello,"+this.name);
+        
+//     }
+// };
+// user.greet(); //Hello,Aman
+  
+// Here:
+// this.name → refers to user.name
  
 
